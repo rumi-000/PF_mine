e294fcb353ff516a81a0ea185ba6bd6a985722c5
rumi-000 <rumi.sato9797@gmail.com>
2023-03-31 03:10:28 +0000

    ポストモデルをマイグレートした

--- db/migrate/20230331022903_create_posts.rb
+++ db/migrate/20230331022903_create_posts.rb
@@ -1,8 +1,17 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      #ここから記載
+      t.integer :user_id,        null:false
+      t.integer :tag_id
+      t.integer :item_name,      null:false
+      t.text:item_description,   null:false
+      #ここまで記載
       t.timestamps
     end
   end
 end
+
+
+
+

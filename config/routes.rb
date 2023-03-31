bcebb5814b7b2817639c857f5c4385994003eef4
rumi-000 <rumi.sato9797@gmail.com>
2023-03-31 02:13:46 +0000

    ユーザーと管理者側のdevice機能が使える様になった

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+  #管理者側　カリキュラム通りに記載している。
+  #app/views/admin/shared/_links.html.erbでコメントアウトしてる。
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  
+  #ユーザー側　カリキュラム通りに記載している。
+  #app/views/user/shared/_links.html.erbでコメントアウトしてる。
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+

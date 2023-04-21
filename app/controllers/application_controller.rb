e103613312b6c587884d88e2fc2d76be38d4f232
rumi-000 <rumi.sato9797@gmail.com>
2023-04-21 08:41:48 +0000

    リンク先訂正できた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,17 @@ class ApplicationController < ActionController::Base
    end
  end
  
+ def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+        #カスタマー側のトップページ(rumi)
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+ end
+
  protected
 
  def configure_permitted_parameters
@@ -16,5 +27,4 @@ class ApplicationController < ActionController::Base
  end
  
 end
-
 

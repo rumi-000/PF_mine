80d183f6e4da320ada9eb9c2e4721412f66c6484
rumi-000 <rumi.sato9797@gmail.com>
2023-04-26 16:12:43 +0000

    修正

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -4,11 +4,5 @@ class PostTag < ApplicationRecord
   belongs_to :tag
   validates :post_id, presence: true
   validates :tag_id, presence: true
-  validate :check_tag_id_published, if: -> { tag_id.present? }
-  
-  def check_post_published
-   unless tag.published?
-    errors.add(:tag, "must be published to add tags")
-   end
-  end
+
 end
